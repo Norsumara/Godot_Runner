1e86f7cffb5b3f6ec67905f36e48084404f17ba7
Martti Salomaa <martti.o.salomaa@gmail.com>
2022-10-05 19:28:04 +0300

    ukko liikkuu vasurii ja oikeeseen & juoksuanimaatio

--- Player.gd
+++ Player.gd
@@ -6,6 +6,8 @@ var step_freq : float = 2.0 #kuinka usein
 var step_height : float = 0.2 # kuinka korkealle maasta
 var step_tilt : float = 8.0 #degrees asteet
 
+onready var body_hinge = $BodyHinge
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -21,4 +23,16 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	move_and_slide(Vector3(0,0,run_speed))
+	body_hinge.translation.y = step_height * (1 + sin(2.0 * PI * step_freq * time))
+	body_hinge.rotation_degrees.z = step_tilt * sin(PI * step_freq *time)
+	time += delta
+	
+	var sideways : float = 0.0
+	
+	if Input.is_action_pressed("move_right"):
+		sideways += 1.0
+		
+	if Input.is_action_pressed("move_left"):
+		sideways -= 1.0
+	
+	move_and_slide(Vector3(sideways * run_speed, 0, run_speed))
